943f33af279f1488c1a899b2e972195b0c11df3e
vrusinov <vrusinov@google.com>
2016-02-09 04:03:58 -0800

    Release python-gflags 3.0.2. 	Change on 2016/02/09 by vrusinov <vrusinov@google.com> ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=114202008

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@
 from setuptools import setup
 
 setup(name='python-gflags',
-      version='3.0.1',
+      version='3.0.2',
       description='Google Commandline Flags Module',
       license='BSD',
       author='Google Inc. and others',
