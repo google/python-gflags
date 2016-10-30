b4cf9cb32db200b3375e142ec8cffd9b2e46d48a
Joshua Boyd <jdboyd@jdboyd.net>
2016-10-30 01:38:20 -0400

    Update Classsifiers
    The purpose is to change the status shown on Python 3 Wall Of Superpowers (https://python3wos.appspot.com/).

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(name='python-gflags',
       requires=['six'],
       classifiers=[
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Libraries :: Python Modules',
